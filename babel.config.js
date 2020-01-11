26edd12e6de421bfbea71e1b0bfe10200ad00d7d
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-01-11 11:54:12 +0900

    chore: apply lint

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
-      "module-resolver",
+      'module-resolver',
       {
         alias: {
-          "@react-native-community/progressview": "./js"
+          '@react-native-community/progressview': './js',
         },
-        cwd: "babelrc"
-      }
-    ]
-  ]
+        cwd: 'babelrc',
+      },
+    ],
+  ],
 };
