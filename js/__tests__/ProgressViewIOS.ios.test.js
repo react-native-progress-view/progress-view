021f7ff8e4832ca0aea9ae1d0426f028613662a4
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-01-24 00:34:16 +0900

    chore: add snapshot tests

---
+++ js/__tests__/ProgressViewIOS.ios.test.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import renderer from 'react-test-renderer';
+import ProgressView from '../ProgressViewIOS.ios';
+
+describe('<ProgressView />', () => {
+  it('renders enabled ProgressView', () => {
+    const tree = renderer.create(<ProgressView />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders Horizontal ProgressView', () => {
+    const tree = renderer.create(<ProgressView progressViewStyle="bar" />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders ProgressView with tintColor', () => {
+    const tree = renderer.create(<ProgressView progressTintColor="red" />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
