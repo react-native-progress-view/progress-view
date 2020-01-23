e8bf61779aa921f54a5c6b510e62378662c51c0a
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-01-24 01:33:15 +0900

    feat: add typescript typings

---
+++ index.d.ts
@@ -0,0 +1,35 @@
+import React from 'react';
+import {ViewProps,ImageSourcePropType} from 'react-native';
+
+export type ProgressViewProps = ViewProps & {
+    /**
+   * The progress bar style.
+   */
+  progressViewStyle?: 'default' | 'bar',
+
+  /**
+   * The progress value (between 0 and 1).
+   */
+  progress?: number,
+
+  /**
+   * The tint color of the progress bar itself.
+   */
+  progressTintColor?: string,
+
+  /**
+   * The tint color of the progress bar track.
+   */
+  trackTintColor?: string,
+
+  /**
+   * A stretchable image to display as the progress bar.
+   */
+  progressImage?: ImageSourcePropType,
+
+  /**
+   * A stretchable image to display behind the progress bar.
+   */
+  trackImage?: ImageSourcePropType,
+}
+export class ProgressView extends React.Component<ProgressViewProps> {}
