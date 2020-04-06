2ab0e20892a61056f4b646e2b5feb22c0587b825
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-04-06 23:01:53 +0900

    chore: update lint config

--- example/index.js
+++ example/index.js
@@ -45,7 +45,7 @@ class ProgressViewExample extends React.Component<Props, State> {
   /* $FlowFixMe(>=0.85.0 site=react_native_fb) This comment suppresses an error
    * found when Flow v0.85 was deployed. To see the error, delete this comment
    * and run Flow. */
-  getProgress = offset => {
+  getProgress = (offset) => {
     const progress = this.state.progress + offset;
     return Math.sin(progress % Math.PI) % 1;
   };
