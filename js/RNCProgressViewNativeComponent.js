193dd5d27971254e5b421088505c38accb0781cb
Nuttapat Kirawittaya <nuttapat.kirawittaya@gmail.com>
2022-07-03 09:58:49 +0800

    feat: update js codegen type

--- js/RNCProgressViewNativeComponent.js
+++ js/RNCProgressViewNativeComponent.js
@@ -9,17 +9,21 @@
 
 'use strict';
 
-import {requireNativeComponent} from 'react-native';
+import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 
-import type {HostComponent} from 'react-native/Libraries/Renderer/shims/ReactNativeTypes';
+import type {HostComponent} from 'react-native';
 import type {ImageSource} from 'react-native/Libraries/Image/ImageSource';
 import type {ColorValue} from 'react-native/Libraries/StyleSheet/StyleSheet';
+import type {
+  Float,
+  WithDefault,
+} from 'react-native/Libraries/Types/CodegenTypes';
 import type {ViewProps} from 'react-native/Libraries/Components/View/ViewPropTypes';
 
 type NativeProps = $ReadOnly<{|
   ...ViewProps,
-  progressViewStyle?: ?('default' | 'bar'),
-  progress?: ?number,
+  progressViewStyle?: WithDefault<'default' | 'bar', 'default'>,
+  progress?: WithDefault<Float, 0>,
   progressTintColor?: ?ColorValue,
   trackTintColor?: ?ColorValue,
   progressImage?: ?ImageSource,
@@ -28,17 +32,17 @@ type NativeProps = $ReadOnly<{|
 
 export type WindowsNativeProps = $ReadOnly<{|
   ...ViewProps,
-  progressViewStyle?: ?('default' | 'bar'),
-  progress?: ?number,
+  progressViewStyle?: WithDefault<'default' | 'bar', 'default'>,
+  progress?: WithDefault<Float, 0>,
   progressTintColor?: ?ColorValue,
   trackTintColor?: ?ColorValue,
   progressImage?: ?ImageSource,
   trackImage?: ?ImageSource,
-  isIndeterminate?: ?Boolean,
+  isIndeterminate?: ?boolean,
 |}>;
 
 type NativeProgressViewIOS = HostComponent<NativeProps>;
 
-export default ((requireNativeComponent(
-  'RNCProgressView',
-): any): NativeProgressViewIOS);
+export default (codegenNativeComponent<NativeProps>('RNCProgressView', {
+  interfaceOnly: true,
+}): NativeProgressViewIOS);
